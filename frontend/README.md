7145cdccdd4ab03fc02ac5157d8fb13e468ef541
apethani <ab823206@dal.ca>
2022-07-03 02:12:24 -0300

    Boilerplate React code for frontend

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+This project is a front end of the serverless application bed and breakfast system.
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      Bed and Breakfast 
+    </div>
+  );
+}
+
+export default App;
