488e663c034e183e2e772b7ba8652689f3e60338
Abhishek Pethani <abhishekpethani@dal.ca>
2022-07-13 20:15:55 -0300

    Code to add user in AWS Cognito user pool is added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Typography } from "@mui/material";
 import SignUp from "./components/UserManagement/SignUp";
+import SignIn from "./components/UserManagement/SignIn";
 
 function App() {
   return (
@@ -7,7 +9,12 @@ function App() {
       <Typography align="center" variant="h4" gutterBottom component="div">
         Bed and Breakfast 
       </Typography>
-      <SignUp />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<SignUp />}  />
+          <Route exact path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
